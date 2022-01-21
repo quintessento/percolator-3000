07e58711d6043bf80775980933d226ad0190f7e5
quintessento <46756767+quintessento@users.noreply.github.com>
2022-01-21 19:55:29 +0100

    Manim percolation
    Added animation rendering of the table with percolation.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from array import array
+from operator import le
 import random
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy as np
+from manim import *
 
 
 class Node:
@@ -19,17 +22,17 @@ class Node:
     def __str__(self):
         return "(row=" + str(self.row) + ", col=" + str(self.col) + ") -> " + str(self.content) 
     
-
-values = []
-
-def build_table(rows: int, cols: int, percolation_value: float) -> None:
+def build_table(rows: int, cols: int, percolation_value: float) -> array:
+    values = []
     for i in range(rows):
         values.append([])
-        for j in range(cols):
+        for _ in range(cols):
             values[i].append((0, 1)[random.uniform(0.0, 1.0) > percolation_value])
+
+    return values
             
 
-def render_table():
+def render_table(values: array) -> None:
     text = ""
     for row in values:
         text += '\n'
@@ -38,11 +41,12 @@ def render_table():
             
     print(text)
     
+    
+def build_trees(values: array) -> array:
+    trees = []
+    rows = len(values)
+    cols = len(values[0])
 
-trees = []
-    
-    
-def build_trees(rows, cols):
     for j in range(cols):
         cell_value = values[0][j]
         if cell_value == 1:
@@ -68,12 +72,18 @@ def build_trees(rows, cols):
     print("Created", len(trees), "trees")
     for tree in trees:
         bt(tree, 0)
-    
-    
-def pour_water():
 
+    return trees
+    
+    
+def pour_water(trees: array, values: array) -> array:
+    path = []
     def traverse(node: Node):
         values[node.row][node.col] = 2
+        coord = (node.row, node.col)
+        if coord not in path:
+            path.append(coord)
+
         node.visited = True
         if node.left_child is not None and not node.left_child.visited:
             traverse(node.left_child)
@@ -85,31 +95,49 @@ def pour_water():
     for tree in trees:
         traverse(tree)
 
+    return path
+
     
-rows = 10
-cols = 10
-build_table(rows, cols, percolation_value = 0.5)
+class PercolationDemo(Scene):
+    
+    def construct(self):
+        values_str = []
+        for i in range(len(values)):
+            chars = []
+            for j in range(len(values[i])):
+                chars.append(str(values[i][j]))
+            values_str.append(chars)
+        print(values_str)
+        table = Table(values_str)
 
-# problematic values
-values = [
-    [1,0,0,1,1,1,1,1,1,1],
-    [1,0,1,1,0,0,1,0,0,0],
-    [1,0,1,1,1,1,1,1,1,1],
-    [1,1,0,1,1,1,1,1,0,0],
-    [1,1,0,0,0,1,1,0,0,1],
-    [1,0,0,1,0,1,0,1,1,1],
-    [0,1,0,1,1,0,0,0,1,0],
-    [0,1,0,0,1,1,0,1,1,1],
-    [1,1,0,1,0,1,0,1,1,0],
-    [0,1,0,0,1,1,1,1,1,1]
-]
+        table = table.scale_to_fit_height(config.frame_height)
+        scale_x = config.frame_width/table.width
+        scale_y = config.frame_height/table.height
+        scale = min(scale_x, scale_y)
+        table = table.scale(scale)
+        
+        scale_ratio = scale_x / scale_y
+        cell_scale = 1.0 / scale_ratio - 0.05
+        self.add(table)
 
+        self.play(table.create())
 
-render_table()
-build_trees(rows, cols)
-pour_water()
-render_table()
+        print("Path length: ", len(path))
+        for coord in path:
+            shifted_coord = (coord[0] + 1, coord[1] + 1)
+            highlight = table.get_highlighted_cell(shifted_coord, color=None)
+            highlight.scale(cell_scale)
+            table.add_to_back(highlight)
+            self.play(highlight.animate.set_color(GREEN), run_time = 0.1) 
 
+        self.wait()
+
+values = build_table(10, 10, percolation_value = 0.5)
+
+render_table(values)
+trees = build_trees(values)
+path = pour_water(trees, values)
+render_table(values)
 
 #data = np.random.randint(3, size = (10, 10))
 
